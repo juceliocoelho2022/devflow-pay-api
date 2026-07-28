3a3ed557812b6e75596cd1770a5a8eb3c0458aed
Jucelio farias Coelho <juceliocoelho2022>
2026-07-28 20:02:37 -0300

    feat(auth): implement user registration and JWT authentication

--- src/main/java/com/devflow/pay/auth/controller/AuthController.java
+++ src/main/java/com/devflow/pay/auth/controller/AuthController.java
@@ -2,8 +2,11 @@ package com.devflow.pay.auth.controller;
 
 import com.devflow.pay.auth.dto.LoginRequest;
 import com.devflow.pay.auth.dto.LoginResponse;
+import com.devflow.pay.auth.dto.RegisterRequest;
+import com.devflow.pay.auth.dto.RegisterResponse;
 import com.devflow.pay.auth.service.AuthService;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,11 +19,18 @@ public class AuthController {
         this.service = service;
     }
 
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public RegisterResponse register(
+            @Valid @RequestBody RegisterRequest request) {
+
+        return service.register(request);
+    }
+
     @PostMapping("/login")
     public LoginResponse login(
             @Valid @RequestBody LoginRequest request) {
 
         return service.login(request);
     }
-
 }
--- src/main/java/com/devflow/pay/auth/service/AuthService.java
+++ src/main/java/com/devflow/pay/auth/service/AuthService.java
@@ -2,31 +2,62 @@ package com.devflow.pay.auth.service;
 
 import com.devflow.pay.auth.dto.LoginRequest;
 import com.devflow.pay.auth.dto.LoginResponse;
+import com.devflow.pay.auth.dto.RegisterRequest;
+import com.devflow.pay.auth.dto.RegisterResponse;
+import com.devflow.pay.auth.mapper.AuthMapper;
+import com.devflow.pay.exception.DuplicateResourceException;
 import com.devflow.pay.security.jwt.JwtService;
-
+import com.devflow.pay.user.entity.User;
+import com.devflow.pay.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AuthService {
 
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
+    private final AuthMapper authMapper;
 
-    public AuthService(AuthenticationManager authenticationManager,
-                       JwtService jwtService) {
+    /**
+     * Cadastro de usuário
+     */
+    public RegisterResponse register(RegisterRequest request) {
 
-        this.authenticationManager = authenticationManager;
-        this.jwtService = jwtService;
+        if (userRepository.existsByEmail(request.email())) {
+            throw new DuplicateResourceException("E-mail já cadastrado.");
+        }
+
+        User user = authMapper.toEntity(request);
+
+        user.setPassword(
+                passwordEncoder.encode(user.getPassword())
+        );
+
+        User saved = userRepository.save(user);
+
+        return authMapper.toResponse(saved);
     }
 
+    /**
+     * Login
+     */
     public LoginResponse login(LoginRequest request) {
 
         authenticationManager.authenticate(
+
                 new UsernamePasswordAuthenticationToken(
+
                         request.getEmail(),
+
                         request.getPassword()
+
                 )
         );
 
